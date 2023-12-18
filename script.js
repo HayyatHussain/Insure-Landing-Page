9a1cce03e3a701b446273c03e50b937eb7967b6c
HayyatHussain <145599914+HayyatHussain@users.noreply.github.com>
2023-12-18 20:26:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -80,8 +80,8 @@ function toggle01 () {
 
     // Animate the text inside the side navigation panel
     gsap.to(".nav #side-nav p", {
-        opacity: 0,
-        y: 150,
+        opacity: 1,
+        y: 0,
         duration: 0.5,
         stagger: 0.15,
         delay: 0.15
